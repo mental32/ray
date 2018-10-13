eb00d046217fe4661203a109bb11524fb887b298
mental <m3nta1@yahoo.com>
2018-10-14 00:31:47 +0100

    bugfix get version correctly

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
+import re
 from distutils.core import setup
 
-with open('ray/__init__.py') as inf:
-    for line in inf:
-        if line.startswith('__version__ = '):
-            version = line.strip('_version =\'')
+def get_version():
+    with open('ray/__init__.py') as inf:
+      match = re.search(r"((\d\.){2,5}\d)", inf.read(), re.MULTILINE)
+
+      if match is None:
+          raise RuntimeError('Version could not be found.')
+    return match.groups()[0]
 
 setup(
     name='ray',
-    version=version,
+    version=get_version(),
     description='Fast asyncronous fortnite api library.'
 )
